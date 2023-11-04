a0912e4cffc4ba921d1e9f68ee418b6eb078efe2
graheed <graheed@gmail.com>
2023-11-04 11:25:55 -0500

    Finished adding Javascript to mailing list of project

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-/* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function () {
+    // Your JavaScript code here
+    const form = document.querySelector(".newsletter form");
+    const messageDiv = document.querySelector(".message");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const emailInput = document.querySelector("#email");
+        const email = emailInput.value.trim();
+
+        if (email === "") {
+            messageDiv.textContent = "Please enter a valid email address.";
+        } else {
+            messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+        }
+    });
+});
